64050e2ac1b10fd71b3a3076fd797e1a19865333
Cemal Erdemir <cerdemir@gmail.com>
2019-07-02 13:23:42 +0300

    http redirection removed

--- Startup.cs
+++ Startup.cs
@@ -80,9 +80,9 @@ namespace farmapi
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
