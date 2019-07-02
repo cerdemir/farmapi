497f57c1f81f8e42a6bde08c1981c7d24a317f18
Cemal Erdemir <cerdemir@gmail.com>
2019-07-02 23:30:30 +0300

    username uniqueindex added

--- Context/FarmApiContext.cs
+++ Context/FarmApiContext.cs
@@ -10,6 +10,14 @@ namespace farmapi.Context
     public class FarmApiContext : DbContext
     {
         public FarmApiContext(DbContextOptions<FarmApiContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            builder.Entity<Product>().HasQueryFilter(e => e.Deleted == false);
+        }
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
     }
